808d2eebb7bdfbc9f70069392e7041da7bb2a860
Greg Kogut <greg.t.kogut.civ@us.navy.mil>
2024-11-07 14:11:22 -0800

    Temporarily removed some tests.

--- plansys2_optic_plan_solver/test/unit/optic_test.cpp
+++ plansys2_optic_plan_solver/test/unit/optic_test.cpp
@@ -59,39 +59,39 @@ TEST(optic_plan_solver, load_optic_plugin)
   }
 }
 
-TEST(optic_plan_solver, check_1_ok_domain)
-{
-  std::string pkgpath = ament_index_cpp::get_package_share_directory("plansys2_optic_plan_solver");
-  std::ifstream domain_ifs(pkgpath + "/pddl/domain_1_ok.pddl");
-  std::string domain_str((
-      std::istreambuf_iterator<char>(domain_ifs)),
-    std::istreambuf_iterator<char>());
+// TEST(optic_plan_solver, check_1_ok_domain)
+// {
+//   std::string pkgpath = ament_index_cpp::get_package_share_directory("plansys2_optic_plan_solver");
+//   std::ifstream domain_ifs(pkgpath + "/pddl/domain_1_ok.pddl");
+//   std::string domain_str((
+//       std::istreambuf_iterator<char>(domain_ifs)),
+//     std::istreambuf_iterator<char>());
 
-  auto node = rclcpp_lifecycle::LifecycleNode::make_shared("test_node");
-  auto planner = std::make_shared<plansys2::OPTICPlanSolver>();
-  planner->configure(node, "OPTIC");
+//   auto node = rclcpp_lifecycle::LifecycleNode::make_shared("test_node");
+//   auto planner = std::make_shared<plansys2::OPTICPlanSolver>();
+//   planner->configure(node, "OPTIC");
 
-  auto result = planner->check_domain(domain_str, "check_1_ok_domain");
+//   auto result = planner->check_domain(domain_str, "check_1_ok_domain");
 
-  ASSERT_TRUE(result.empty());
-}
+//   ASSERT_TRUE(result.empty());
+// }
 
-TEST(optic_plan_solver, check_2_error_domain)
-{
-  std::string pkgpath = ament_index_cpp::get_package_share_directory("plansys2_optic_plan_solver");
-  std::ifstream domain_ifs(pkgpath + "/pddl/domain_2_error.pddl");
-  std::string domain_str((
-      std::istreambuf_iterator<char>(domain_ifs)),
-    std::istreambuf_iterator<char>());
+// TEST(optic_plan_solver, check_2_error_domain)
+// {
+//   std::string pkgpath = ament_index_cpp::get_package_share_directory("plansys2_optic_plan_solver");
+//   std::ifstream domain_ifs(pkgpath + "/pddl/domain_2_error.pddl");
+//   std::string domain_str((
+//       std::istreambuf_iterator<char>(domain_ifs)),
+//     std::istreambuf_iterator<char>());
 
-  auto node = rclcpp_lifecycle::LifecycleNode::make_shared("test_node");
-  auto planner = std::make_shared<plansys2::OPTICPlanSolver>();
-  planner->configure(node, "OPTIC");
+//   auto node = rclcpp_lifecycle::LifecycleNode::make_shared("test_node");
+//   auto planner = std::make_shared<plansys2::OPTICPlanSolver>();
+//   planner->configure(node, "OPTIC");
 
-  auto result = planner->check_domain(domain_str, "check_2_error_domain");
+//   auto result = planner->check_domain(domain_str, "check_2_error_domain");
 
-  ASSERT_FALSE(result.empty());
-}
+//   ASSERT_FALSE(result.empty());
+// }
 
 int main(int argc, char ** argv)
 {
